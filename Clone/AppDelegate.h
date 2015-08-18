abaf3ee924796adb6b6edc55a7c2c3812248add4
Deja Jackson <TheDeej@Dejas-MacBook-Air.local>
2015-08-17 19:34:18 -0700

    Initial Commit

---
+++ Clone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clone
+//
+//  Created by Deja Jackson on 8/17/15.
+//  Copyright (c) 2015 Deja Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
